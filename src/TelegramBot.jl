8122e2cc28aaa00cbd4debfe50b7702d68f0a7db
i7242 <xingyuyan@outlook.com>
2022-12-17 12:15:34 -0500

    add auto confirm

--- src/TelegramBot.jl
+++ src/TelegramBot.jl
@@ -10,16 +10,17 @@ module TelegramBot
   get_TGBot() |>
     get_latest_update |>
     handle_subscription! |>
-    handle_random_response
+    handle_random_response |>
+    handle_confirm_update
 =#
 
 using HTTP, JSON, ConfigEnv
 
 export TGBot,TGParcel
-export get_TGBot, get_latest_update, handle_subscription!, handle_random_response
+export get_TGBot, get_latest_update, handle_subscription!, handle_random_response, handle_confirm_update
 
 const BOT_URL_PRE="https://api.telegram.org/bot"
-const SUB="SUB"
+const SUB="/SUB"
 
 """
 TGBot
@@ -52,27 +53,20 @@ function get_TGBot()::TGBot
 end
 
 """
-Only get the latest update by set `offset=-1`. Any message in between will be ignored.
+Only get the latest one update by set `offset=-1&limit=1`. Any message in between will be ignored.
 Original response body parsed to JSON will be returned.
 """
 function get_latest_update(bot::TGBot)::TGParcel
-  HTTP.get(BOT_URL_PRE*bot.api_key*"/getUpdates?offset=-1").body |>
+  HTTP.get(BOT_URL_PRE*bot.api_key*"/getUpdates?offset=-1&limit=1").body |>
     String |>
     JSON.parse |>
     body -> TGParcel(bot, body)
 end
 
 """
-Check if we have new message.
-"""
-function parcel_has_message(parcel::TGParcel)::Bool
-  parcel.body["ok"] && !isempty(parcel.body["result"])
-end
-
-"""
 Get message text from parcel.
   - assume we do have message inside
-  - use this after parcel_has_message()
+  - only use this after parcel empty check
 """
 function get_parcel_message(parcel::TGParcel)::String
   parcel.body["result"][end]["message"]["text"]
@@ -81,10 +75,18 @@ end
 """
 Get chat id.
   - assume get only one message form one client...
-  - use this after parcel_has_message()
+  - only use this after parcel empty check
 """
 function get_chat_id(parcel::TGParcel)::String
   parcel.body["result"][end]["message"]["chat"]["id"] |> string
+end
+
+"""
+Get update id.
+  - only use this after parcel empty check
+"""
+function get_update_id(parcel::TGParcel)::String
+  parcel.body["result"][end]["update_id"] |> string
 end
 
 """
@@ -92,10 +94,9 @@ Add chat id to subscription if got message "SUB!".
   - this will modify the array of chat_id inside the bot
 """
 function handle_subscription!(parcel::TGParcel)::TGParcel
-  if parcel_has_message(parcel)
-    if (SUB == get_parcel_message(parcel))
-      push!(parcel.bot.chat_ids, get_chat_id(parcel))
-    end
+  isempty(parcel.body["result"]) && return parcel
+  if (SUB == get_parcel_message(parcel))
+    push!(parcel.bot.chat_ids, get_chat_id(parcel))
   end
   parcel
 end
@@ -104,10 +105,25 @@ end
 Send a random number.
 """
 function handle_random_response(parcel::TGParcel)::TGParcel
+  isempty(parcel.body["result"]) && return parcel
   HTTP.post(BOT_URL_PRE*parcel.bot.api_key*
               "/sendMessage?chat_id="*
               get_chat_id(parcel)*"&text="*string(rand()))
   parcel
+end
+
+"""
+Send a request with next offset id so current message get confirmed.
+- limit the number of TG API return to be only one
+- return a new TGParcel with the new TG response, possiblly empty
+"""
+function handle_confirm_update(parcel::TGParcel)::TGParcel
+  isempty(parcel.body["result"]) && return parcel
+  next_id = string(parse(Int64, get_update_id(parcel))+1)
+  HTTP.get(BOT_URL_PRE*parcel.bot.api_key*"/getUpdates?offset=$next_id&limit=1").body |>
+    String |>
+    JSON.parse |>
+    body -> TGParcel(parcel.bot, body)
 end
 
 end # module
